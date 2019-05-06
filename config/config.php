ef36da901e4c7de50f75e8fc92a525d3e00a0cb1
root <root@pi-star>
2019-05-06 21:27:58 +0100

    added color

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 # This is an auto-generated config-file!
 # Be careful, when manual editing this!
 
-date_default_timezone_set('UTC');
+date_default_timezone_set('Europe/London');
 define("MMDVMLOGPATH", "/var/log/pi-star");
 define("MMDVMLOGPREFIX", "MMDVM");
 define("MMDVMINIPATH", "/etc");
